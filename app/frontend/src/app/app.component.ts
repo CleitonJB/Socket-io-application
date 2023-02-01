8d9446334c07c8ed9e3cbb6cb2ffc96ebd32ac70
Cleiton Braga <cleitonbraga56@gmail.com>
2023-01-31 22:36:02 -0300

    Connect with socket.io local server And Allow to receive and send messages

--- app/frontend/src/app/app.component.ts
+++ app/frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { ChatService } from './chat/chat.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  public newMessage: string = '';
+  public messageList: string[] = [];
+
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.chatService.getNewMessage().subscribe({
+      next: (message: string) => {
+        this.messageList.push(message);
+      }
+    });
+  }
+
+  public sendMessage(): void {
+    this.chatService.sendMessage(this.newMessage);
+    this.newMessage = '';
+  }
 }
---
+++ app/frontend/src/app/chat/chat.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { io } from "socket.io-client";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  public $message: BehaviorSubject<string> = new BehaviorSubject('');
+
+  socket = io('http://localhost:3333');
+
+  constructor() { }
+
+  public sendMessage(message: string): void {
+    this.socket.emit('message', message);
+  }
+
+  public getNewMessage = () => {
+    this.socket.on('message', (message) => {
+      this.$message.next(message);
+    });
+
+    return this.$message.asObservable();
+  }
+}
